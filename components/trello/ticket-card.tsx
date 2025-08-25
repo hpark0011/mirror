24b62c0f0d527241e07ea3670631c20501380fbd
Hyunsol Park <hpark0011@gmail.com>
2025-08-25 10:43:29 +0900

    work on status variant styles

--- components/trello/ticket-card.tsx
+++ components/trello/ticket-card.tsx
@@ -41,12 +41,22 @@ export function TicketCard({
     opacity: isSortableDragging ? 0.5 : 1,
   };
 
+  const statusStyles = {
+    backlog: "bg-white/40 border-white/40 hover:bg-base",
+    "not-started": "bg-white/40 border-white/40 hover:bg-base",
+    "in-progress":
+      "bg-white/70 border-white/40 shadow-[0_8px_8px_-4px_rgba(255,255,255,0.9),_0_12px_12px_-6px_rgba(0,0,0,0.3)] hover:shadow-[0_24px_24px_-12px_rgba(255,255,255,0.9),_0_24px_24px_-12px_rgba(0,0,0,0.3)] hover:bg-base",
+    complete:
+      "bg-white/30 border-white/30 row-span-full row-start-1 hidden border-x bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:col-start-3 md:block dark:[--pattern-fg:var(--color-white)]/10",
+  };
+
   return (
     <Card
       ref={setNodeRef}
       style={style}
       className={cn(
-        "relative border border-white/40 transition-all duration-200 translate-y-0 hover:translate-y-[-1px] scale-100 hover:scale-[1.02] ease-out group cursor-grab active:cursor-grabbing p-0 gap-0 bg-white/40 hover:bg-base hover:border-white/100 inset-shadow-none shadow-xs hover:shadow-[0_12px_12px_-6px_rgba(255,255,255,0.9),_0_14px_14px_-6px_rgba(0,0,0,0.3)]",
+        "relative border transition-all duration-200 translate-y-0 hover:translate-y-[-1px] scale-100 hover:scale-[1.02] ease-out group cursor-grab active:cursor-grabbing p-0 gap-0 hover:border-opacity-100 inset-shadow-none shadow-xs hover:shadow-[0_12px_12px_-6px_rgba(255,255,255,0.9),_0_14px_14px_-6px_rgba(0,0,0,0.3)]",
+        statusStyles[ticket.status],
         isDragging &&
           "rotate-5 scale-105 shadow-[0_12px_12px_-6px_rgba(255,255,255,0.9),_0_14px_14px_-6px_rgba(0,0,0,0.3)]"
       )}
