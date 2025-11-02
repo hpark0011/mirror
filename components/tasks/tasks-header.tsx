3e8a44b9c9d56d2baf74e02404f9582d1b75cb70
Hyunsol Park <hpark0011@gmail.com>
2025-11-02 16:31:30 +0900

    solve border color issue

--- components/tasks/tasks-header.tsx
+++ components/tasks/tasks-header.tsx
@@ -65,11 +65,12 @@ export function TasksHeader({ onImport, onClear }: HeaderProps) {
         <BreadcrumbList className='items-center text-[14px] text-foreground sm:gap-0'>
           <BreadcrumbItem>
             <BreadcrumbLink asChild>
-              <div className='p-[4px] px-[5px] border duration-300 ease-out transition-all border-transparent hover:border-border-highlight hover:shadow-[inset_0_0_4px_1px_rgba(255,255,255,1),_0_4px_16px_-8px_rgba(0,0,0,0.2)] hover:dark:shadow-[inset_0_0_4px_1px_rgba(0,0,0,1),_0_4px_16px_-8px_rgba(255,255,255,0.2)] dark:hover:border-neutral-800 rounded-full bg-gradient-to-t hover:from-black/15 hover:to-white hover:dark:from-white/30 hover:dark:to-black'>
-                <Link href='/' aria-label='Go to home'>
-                  <HeaderLogo />
-                </Link>
-              </div>
+              {/* <div className='p-[4px] px-[5px] border duration-300 ease-out transition-all border-transparent hover:border-border-highlight hover:shadow-[inset_0_0_4px_1px_rgba(255,255,255,1),_0_4px_16px_-8px_rgba(0,0,0,0.2)] hover:dark:shadow-[inset_0_0_4px_1px_rgba(0,0,0,1),_0_4px_16px_-8px_rgba(255,255,255,0.2)] dark:hover:border-neutral-800 rounded-full bg-gradient-to-t hover:from-black/15 hover:to-white hover:dark:from-white/30 hover:dark:to-black'> */}
+
+              <Link href='/' aria-label='Go to home'>
+                <HeaderLogo />
+              </Link>
+              {/* </div> */}
             </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator className='text-neutral-400/50 pt-0.5 dark:text-neutral-700 [&>svg]:!size-5 ml-0.5 mr-[-4px] '>
