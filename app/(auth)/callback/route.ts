d6c8faf8a349b8f95f377f6c24891c43d91408b1
Hyunsol Park <hpark0011@gmail.com>
2025-11-03 08:58:13 +0900

    solve lint errr

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -1,10 +1,8 @@
 import { redirect } from "next/navigation";
 import type { NextRequest } from "next/server";
-
+import { PATHS } from "@/config/paths.config";
 import { createAuthCallbackService } from "@/lib/services/auth-callback.service";
 import { getSupabaseServerClient } from "@/utils/supabase/client/supabase-server";
-
-import { PATHS } from "@/config/paths.config";
 
 export async function GET(request: NextRequest) {
   const service = createAuthCallbackService(await getSupabaseServerClient());
