932831c1480e8335386252e5a95260e84a2b8edc
Hyunsol Park <hpark0011@gmail.com>
2026-02-18 21:06:31 +0900

    modify greyboard desktop app

--- apps/greyboard-desktop/src/App.tsx
+++ apps/greyboard-desktop/src/App.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router-dom'
-import { ErrorBoundary } from './components/error-boundary'
+import { Outlet } from "react-router-dom";
+import { ErrorBoundary } from "./components/error-boundary";
 
 export function App() {
   return (
@@ -13,5 +13,5 @@ export function App() {
         </ErrorBoundary>
       </main>
     </div>
-  )
+  );
 }
